6380bc0f72ea82ce4dc548c5147844a83a77aeb5
Franco Bossi <fbossi2003@gmail.com>
2025-04-02 15:37:29 -0300

    "Pagina web

---
+++ PAGINA/funciones.js
@@ -0,0 +1,7 @@
+document.getElementById("miBoton").addEventListener("click", function() {
+    alert("¡Botón clickeado!");
+});
+
+document.getElementById("Shooter").addEventListener("click", function() {
+    alert("¡Shooter clickeado!");
+});
